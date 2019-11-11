4e0e17c5a119b2bd5e104c1b0d10334bce489bcd
simotae14 <startae14@gmail.com>
2019-11-12 00:09:08 +0100

    partial

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import {ThemeProvider} from 'emotion-theming'
 import Calculator from './calculator'
+import * as themes from './themes'
 
 function App() {
+  state = {theme: 'dark'}
+  handleThemeChange = ({target: {value}}) => this.setState({theme: value})
   return (
-    <React.Fragment>
-      <Calculator />
-      <div style={{marginTop: 30, textAlign: 'center'}}>
-        Calculator component{' '}
-        <a href="https://codepen.io/mjijackson/pen/xOzyGX">created</a>
-        {' by '}
-        <br />
-        <a href="https://twitter.com/mjackson">Michael Jackson</a> of{' '}
-        <a href="https://reacttraining.com/">React Training</a>
-      </div>
-    </React.Fragment>
+    <ThemeProvider theme={themes[this.state.theme]}>
+      <React.Fragment>
+        <Calculator />
+        <div style={{marginTop: 30, textAlign: 'center'}}>
+          Calculator component{' '}
+          <a href="https://codepen.io/mjijackson/pen/xOzyGX">created</a>
+          {' by '}
+          <br />
+          <a href="https://twitter.com/mjackson">Michael Jackson</a> of{' '}
+          <a href="https://reacttraining.com/">React Training</a>
+        </div>
+      </React.Fragment>
+    </ThemeProvider>
   )
 }
 
