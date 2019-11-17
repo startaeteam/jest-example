f1d55ffd7926da3cf9b3286af2f60f3a4d44f527
Simone Taeggi <startae14@gmail.com>
2019-11-17 20:09:24 +0100

    set threshold for the global and singular coverage of our tests

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,18 @@ module.exports = {
   },
   setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 17,
+      branches: 4,
+      lines: 17,
+      functions: 20
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    }
+  },
 }
